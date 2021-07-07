e4d362c4412f8b213cf8f887e8bfe1a3df7fc953
willyserge <willyserge01@gmail.com>
2021-07-07 15:26:10 +0200

    component structure

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './Components/Task';
 
 export default function App() {
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <View style={styles.container}>
-      <Text>react native todo</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>section title</Text>
+        <View style={styles.items}>
+          <Task />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+
   },
 });
---
+++ Components/Task.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Task = () => (
+  <View>
+    <Text>This is a task</Text>
+  </View>
+);
+
+export default Task;
